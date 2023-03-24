de913a69545f116db045a31e90fdef3f90bb9e11
Diego Henrique <diego.reis.ruiz@gmail.com>
2023-03-24 00:46:35 -0300

    subindo o projeto já pronto, não subi por partes pois não tinha certeza se ia dar ceto kkkkk

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function convertDecimal(numDec) {
+    var decimal = parseInt(numDec)
+    var binary = decimal.toString(2)
+    document.getElementById("resultBinary").innerText = binary
+}
+
+function convertBinary() {
+    var binary = document.getElementById("textDecimal").value
+    var result = document.getElementById("resultDecimal")
+    decNum = 0
+    for (let i = binary.length; i > 0; i--) {
+        let cc = binary.charAt(i - 1)
+        if (cc == 1) {
+            decNum += Math.pow(2, binary.length - i)
+        }
+    }
+    result.textContent = decNum
+}
